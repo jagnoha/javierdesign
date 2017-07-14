4b160cb72f9c67c7dde69c3a260bb563317afc4f
Ubuntu <ubuntu@ip-172-31-0-92.us-west-2.compute.internal>
2017-07-14 05:00:53 +0000

    Starting website

---
+++ sites/default/files/php/twig/59682f579bd4b_block.html.twig_GS6B6uXfSHbJK2lNaZh2x3dSJ/ABtEcNGXZw7swuCDDBaMVZomQsCE5NFD-Bur8ALSthY.php
@@ -0,0 +1,161 @@
+<?php
+
+/* themes/stem_theme/templates/block/block.html.twig */
+class __TwigTemplate_f9d2f0b8c6d02ec85f90d0430c84657bd3c8620011bc48d2586aac4dc3ab5b16 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("set" => 29, "if" => 39, "block" => 45);
+        $filters = array("clean_class" => 31);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array('set', 'if', 'block'),
+                array('clean_class'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 29
+        $context["classes"] = array(0 => "block", 1 => ("block-" . \Drupal\Component\Utility\Html::getClass($this->getAttribute(        // line 31
+(isset($context["configuration"]) ? $context["configuration"] : null), "provider", array()))), 2 => ("block-" . \Drupal\Component\Utility\Html::getClass(        // line 32
+(isset($context["plugin_id"]) ? $context["plugin_id"] : null))));
+        // line 35
+        echo "
+<div";
+        // line 36
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "addClass", array(0 => (isset($context["classes"]) ? $context["classes"] : null)), "method"), "html", null, true));
+        echo " >
+<div class=\"container-wrap clearfix\">
+  ";
+        // line 38
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["title_prefix"]) ? $context["title_prefix"] : null), "html", null, true));
+        echo "
+  ";
+        // line 39
+        if ((isset($context["label"]) ? $context["label"] : null)) {
+            // line 40
+            echo "\t<div class=\"row block-title-wrap\">
+\t<h2";
+            // line 41
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["title_attributes"]) ? $context["title_attributes"] : null), "addClass", array(0 => "block-title"), "method"), "html", null, true));
+            echo ">";
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["label"]) ? $context["label"] : null), "html", null, true));
+            echo "</h2>
+\t</div>
+  ";
+        }
+        // line 44
+        echo "  ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["title_suffix"]) ? $context["title_suffix"] : null), "html", null, true));
+        echo "
+  ";
+        // line 45
+        $this->displayBlock('content', $context, $blocks);
+        // line 48
+        echo "</div>
+</div>";
+    }
+
+    // line 45
+    public function block_content($context, array $blocks = array())
+    {
+        // line 46
+        echo "      ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["content"]) ? $context["content"] : null), "html", null, true));
+        echo "
+  ";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/stem_theme/templates/block/block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  88 => 46,  85 => 45,  80 => 48,  78 => 45,  73 => 44,  65 => 41,  62 => 40,  60 => 39,  56 => 38,  51 => 36,  48 => 35,  46 => 32,  45 => 31,  44 => 29,);
+    }
+
+    public function getSource()
+    {
+        return "{#
+/**
+ * @file
+ * Theme override to display a block.
+ *
+ * Available variables:
+ * - plugin_id: The ID of the block implementation.
+ * - label: The configured label of the block if visible.
+ * - configuration: A list of the block's configuration values.
+ *   - label: The configured label for the block.
+ *   - label_display: The display settings for the label.
+ *   - provider: The module or other provider that provided this block plugin.
+ *   - Block plugin specific settings will also be stored here.
+ * - content: The content of this block.
+ * - attributes: array of HTML attributes populated by modules, intended to
+ *   be added to the main container tag of this template.
+ *   - id: A valid HTML ID and guaranteed unique.
+ * - title_attributes: Same as attributes, except applied to the main title
+ *   tag that appears in the template.
+ * - title_prefix: Additional output populated by modules, intended to be
+ *   displayed in front of the main title tag that appears in the template.
+ * - title_suffix: Additional output populated by modules, intended to be
+ *   displayed after the main title tag that appears in the template.
+ *
+ * @see template_preprocess_block()
+ */
+#}
+{%
+  set classes = [
+    'block',
+    'block-' ~ configuration.provider|clean_class,
+    'block-' ~ plugin_id|clean_class,
+  ]
+%}
+
+<div{{ attributes.addClass(classes) }} >
+<div class=\"container-wrap clearfix\">
+  {{ title_prefix }}
+  {% if label %}
+\t<div class=\"row block-title-wrap\">
+\t<h2{{ title_attributes.addClass('block-title') }}>{{ label }}</h2>
+\t</div>
+  {% endif %}
+  {{ title_suffix }}
+  {% block content %}
+      {{ content }}
+  {% endblock %}
+</div>
+</div>";
+    }
+}
